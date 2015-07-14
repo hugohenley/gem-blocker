7f251481a23f8d076d80e0b06f260ed1a20d588c
Hugo Henley <hugohenley@gmail.com>
2015-07-14 18:52:51 -0300

    show project commits

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,6 +3,10 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all
   end
+  
+  def show
+    @project = Project.find(params[:id])
+  end
 
   def new
     @project = Project.new
--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,5 @@
 module ProjectsHelper
+  def last_commit_time(project)
+    project.commits.last.commit_created_at if project.commits.any?
+  end
 end
