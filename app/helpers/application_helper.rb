27cff5fec1e610a85181792b65f4131a1c023d20
Hugo Henley <hugohenley@gmail.com>
2015-07-21 19:00:27 -0300

    removing flash method helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,3 @@
 module ApplicationHelper
 
-  def bootstrap_class_for flash_type
-    pry
-    case flash_type.to_sym
-      when :notice
-        "success"
-      when :success
-        "success"
-      when :error
-        "danger"
-      when :warning
-        "warning"
-      when :info
-        "info"
-      else
-        flash_type.to_s
-    end
-  end
-
-
 end
