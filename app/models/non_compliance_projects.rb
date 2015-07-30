015d1b89d4d5f9f21e42691d3ff5cad4eed6ec87
Hugo Henley <hugohenley@gmail.com>
2015-07-29 23:46:19 -0300

    refactoring list method

--- app/models/non_compliance_projects.rb
+++ app/models/non_compliance_projects.rb
@@ -3,21 +3,26 @@ class NonComplianceProjects
   def list
     projects = []
     Project.all.each do |project|
-      project_with_gems = {}
-      used_gems = last_used_gems_of project
-      next if used_gems.nil?
-      project_with_gems["#{project.name}"] = []
-      required_gems = verify_locked_gems(used_gems, :required)
-      allowed_gems = verify_locked_gems(used_gems, :allow_if_present)
-      denied_gems = verify_locked_gems(used_gems, :deny)
-
-      project_with_gems["#{project.name}"] << required_gems
-      project_with_gems["#{project.name}"] << allowed_gems
-      project_with_gems["#{project.name}"] << denied_gems
-
+      project_with_gems = verify_project project
+      next if project_with_gems.nil?
       projects << project_with_gems unless project_with_gems["#{project.name}"].empty?
     end
     projects
+  end
+
+  def verify_project project
+    project_with_gems = {}
+    used_gems = last_used_gems_of project
+    return if used_gems.nil?
+    project_with_gems["#{project.name}"] = []
+    required_gems = verify_locked_gems(used_gems, :required)
+    allowed_gems = verify_locked_gems(used_gems, :allow_if_present)
+    denied_gems = verify_locked_gems(used_gems, :deny)
+
+    project_with_gems["#{project.name}"] << required_gems
+    project_with_gems["#{project.name}"] << allowed_gems
+    project_with_gems["#{project.name}"] << denied_gems
+    project_with_gems
   end
 
   def last_used_gems_of project
@@ -33,7 +38,7 @@ class NonComplianceProjects
   end
 
   def verify_locked_gems(used_gems, type)
-    locked_gems = { type => { } }
+    locked_gems = {type => {}}
     required_gems = Gemblocker.hash_of type.to_s.titleize
     required_gems.each do |required_gem|
       gem_name = required_gem.keys.first
