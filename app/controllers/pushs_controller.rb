28415fe5dbf2abee3709553d44ff494ebb2a7b5c
Hugo Henley <hugohenley@gmail.com>
2015-08-01 23:51:41 -0300

    wrong name on instance variable

--- app/controllers/pushs_controller.rb
+++ app/controllers/pushs_controller.rb
@@ -2,8 +2,8 @@ class PushsController < ApplicationController
 
   def create
     hook_params = PushHookParser.new(params)
-    @commit = Push.new(hook_params)
-    if @commit.save
+    @push = Push.new(hook_params)
+    if @push.save
       render :nothing => true, :status => :ok
     else
       render :nothing => true, :status => :internal_server_error
